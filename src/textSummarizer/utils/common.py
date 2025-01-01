6b3b6da908c80f2b84d803062f50c29d00aa1f0b
mayank3354 <mayankkhs@gmail.com>
2025-01-01 16:15:19 +0530

    training done

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -19,6 +19,8 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
     except Exception as e:
         raise e
+
+        
 @ensure_annotations
 def create_directories(path_to_directories: list,verbose=True):
     for path in path_to_directories:
