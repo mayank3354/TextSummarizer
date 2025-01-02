2951d3d68d4e9b0dcdd88c60fc91d13f5928b071
mayank3354 <mayankkhs@gmail.com>
2025-01-02 12:42:58 +0530

    Data Ingestion Completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# TextSummarizer
+# TextSummarizer Using HuggingFace
+
+
+### WORKflows
+
+1. Config.yaml
+2. Params.yaml
+3. Config Entity
+4. Configuration Manager
+5. Update the components - Data Ingestion , Data Transformation , Model Trainer
+6. Create our Pipeline --- Training Pipeline, Prediction Pipeline
+7. Front end -- Api's, Training Api's,Batch Prediction Api's
+
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
 
-logger.info("Logging is Implemented")
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f"Initiating {STAGE_NAME}")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"{STAGE_NAME} completed successfully")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
