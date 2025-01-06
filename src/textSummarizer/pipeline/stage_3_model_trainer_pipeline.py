67100e23e8210c6c002409464839b3347a2f2e1e
mayank3354 <mayankkhs@gmail.com>
2025-01-06 23:59:07 +0530

    Training completed with bert base on local

--- src/textSummarizer/pipeline/stage_3_model_trainer_pipeline.py
+++ src/textSummarizer/pipeline/stage_3_model_trainer_pipeline.py
@@ -1,5 +1,4 @@
 from src.textSummarizer.config.configuration import ConfigurationManager
-from src.textSummarizer.components.model_trainer import ModelTrainerConfig
 from src.textSummarizer.logging import logger
 
 from src.textSummarizer.components.model_trainer import ModelTrainer
